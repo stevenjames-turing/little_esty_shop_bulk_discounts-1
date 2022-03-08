4d97f9aede578a0b21c03069344c36920586c7ab
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-07 20:59:57 -0600

    Refactor best_discount for accuracy

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -16,8 +16,8 @@ class InvoiceItem < ApplicationRecord
   end
 
   def best_discount
-    InvoiceItem.joins(item: [merchant: :bulk_discounts])
-    .where("invoice_items.quantity >= bulk_discounts.threshold and items.id = #{item_id}")
+    InvoiceItem.joins(:invoice, item: [merchant: :bulk_discounts])
+    .where("invoice_items.quantity >= bulk_discounts.threshold and items.id = #{item_id} and invoices.id = #{invoice_id}")
     .select("bulk_discounts.discount, bulk_discounts.id")
     .order("bulk_discounts.discount desc")
     .limit(1)
