145ae2555093ce2b451194626a6a7431b3b11cb9
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-07 18:24:54 -0600

    Update API service to create holiday records

--- app/services/holiday_api.rb
+++ app/services/holiday_api.rb
@@ -5,8 +5,16 @@ class HolidayApi
   end
 
   def upcoming_holidays(count)
+
     resp = conn.get("api/v3/NextPublicHolidays/US")
     json = JSON.parse(resp.body, symbolize_names: true)
+    json.each do |holiday|
+      existing_holiday = Holiday.where(name: holiday[:name], date: holiday[:date])
+      if existing_holiday.count == 0
+        Holiday.create!(name: holiday[:name], 
+                        date: holiday[:date])
+      end
+    end
     json[0...count]
   end
 end
