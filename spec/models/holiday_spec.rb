532f2eebfc904114865a5ce304c183085be453f2
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-08 11:26:56 -0600

    Add Holiday class method model testing

--- spec/models/holiday_spec.rb
+++ spec/models/holiday_spec.rb
@@ -10,5 +10,26 @@ RSpec.describe Holiday, type: :model do
     it { have_many :bulk_discounts }
   end
 
-  
+  before(:each) do 
+    
+  end
+
+  describe 'class methods' do 
+    describe '#upcoming_holidays(count)' do 
+      it 'returns the next upcoming holidays with count based upon arg passed' do
+        api = HolidayApi.new.get_holidays
+        upcoming_holidays = Holiday.upcoming_holidays(3)
+        holiday1 = upcoming_holidays[0]
+        holiday2 = upcoming_holidays[1]
+        holiday3 = upcoming_holidays[2]
+        expect(upcoming_holidays.count).to eq(3)
+        expect(upcoming_holidays).to eq([holiday1, holiday2, holiday3])
+        expect(holiday1).to_not eq(holiday2)
+        expect(holiday1).to_not eq(holiday3)
+        expect(holiday2).to_not eq(holiday3)
+      end 
+    end
+  end
+
+ 
 end 
